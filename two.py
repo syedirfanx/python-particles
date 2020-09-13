71a18bec335a5c3a18809cac4339de8ecf44068b
Erfanx <irfansyed479@gmail.com>
2020-09-13 16:13:36 +0600

    Make large number easier to read

---
+++ two.py
@@ -0,0 +1,8 @@
+# Underscore make number easier to read and doesn't effect in values
+num1 = 100_000_000
+num2 = 1_000_000
+
+total = num1 + num2
+
+# String formatting using f-string
+print(f'{total:,}')
